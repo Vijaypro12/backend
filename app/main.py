38a3400093650e55663e2c187fdbd37e5917db91
VIJAY <144886505+Vijaypro12@users.noreply.github.com>
2026-09-08 11:21:54 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -34,11 +34,11 @@ def health():
     return {
         "status": "healthy"
     }
-
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]    
+    "https://promptstudio-tawny.vercel.app",
+
+]
 
 app.add_middleware(
     CORSMiddleware,
